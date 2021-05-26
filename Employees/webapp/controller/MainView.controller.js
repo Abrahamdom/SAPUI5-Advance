40fabbbd6184b6e55f53bec562cedd75bf4cff95
Abrahamdom <abrahamdom.sap@gmail.com>
2021-05-26 16:57:52 +0000

    First Commit 04

--- Employees/webapp/controller/MainView.controller.js
+++ Employees/webapp/controller/MainView.controller.js
@@ -1,3 +1,4 @@
+//@ts-nocheck
 sap.ui.define([
 		"sap/ui/core/mvc/Controller"
 	],
@@ -10,6 +11,23 @@ sap.ui.define([
 		return Controller.extend("abrahamgroup.Employees.controller.MainView", {
 			onInit: function () {
 
-			}
+            },
+            
+            onValidate: function(){
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if(valueEmployee.length === 8){
+                    //inputEmployee.setDescription("Ok");
+                    this.byId("labelCountry").setVisible(true);
+                    this.byId("slCountry").setVisible(true);
+                }else{
+                    //inputEmployee.setDescription("Not Ok");
+                    this.byId("labelCountry").setVisible(false);
+                    this.byId("slCountry").setVisible(false);
+                }
+
+            }
+
 		});
 	});
